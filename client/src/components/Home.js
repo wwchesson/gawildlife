2068405c8992599960edc04ec6be4581756b7dea
Will <willchesson@gmail.com>
2023-03-10 13:33:34 -0500

    home photo

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,27 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
 import { HomeStyle } from "./StyleElements";
+import Deer from "./Deer.jpeg"
 
-function Home({animals}) {
-  const [homePhoto, setHomePhoto] = useState([]);
+function Home({}) {
 
-  useEffect(() => {
-    fetch(`/animals/${1}`)
-      .then((r) => r.json())
-      .then((photo) => {
-        setHomePhoto(photo.image);
-        console.log(homePhoto)
-      });
-  }, []);
-
-  // const photoToDisplay = homePhoto.map((photo) => (
-  //   <img className="deer" key={photo.id} src={photo.image} alt="home"></img>
-  // ));
 
   return (
     <HomeStyle>
       <h1 className="wildlife">Georgia Wildlife</h1>
-      <img className ="deer" src={homePhoto}></img>
+      <img className ="deer" src={Deer}></img>
       <h3 className="intro">
         Learn about naturally occuring wildlife in the state of Georgia.
       </h3>
